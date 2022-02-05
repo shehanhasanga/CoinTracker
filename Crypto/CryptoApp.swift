bb6f72c615c918632eed0b4110df1548b4bd9862
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-02-05 08:33:45 +0530

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by shehan karunarathna on 2022-02-05.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
