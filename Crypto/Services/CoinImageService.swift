96ebb458b33891780f5e489a34d38f1731be9ff5
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-02-05 20:39:54 +0530

    save images to local storage

--- Crypto/Services/CoinImageService.swift
+++ Crypto/Services/CoinImageService.swift
@@ -13,13 +13,28 @@ class CoinImageService{
     @Published var image : UIImage? = nil
     var imageSubscription : AnyCancellable?
     private var coin:CoinModel
+    private let localFileManger = LocalFileManager.instance
+    private let folderName = "coin_images"
+    private let imageName:String
     
     
     init(coin: CoinModel){
         self.coin = coin
-        getImage()
+        self.imageName = coin.id ?? ""
+        getCoinImage()
     }
-    private func getImage(){
+    
+    private func getCoinImage(){
+        if let savedImage = localFileManger.getImage(imageName: imageName, folderName: folderName) {
+            image = savedImage
+            print("Get image from local file manager.")
+        } else {
+            downloadCoinImage()
+            print("Download Image.")
+        }
+    }
+    
+    private func downloadCoinImage(){
         guard let url = URL(string: coin.image ?? "") else { return  }
         
         imageSubscription =  NetworkingManager.fetchCoins(url: url)
@@ -28,9 +43,11 @@ class CoinImageService{
             })
             .sink(receiveCompletion: { completion in
                 NetworkingManager.handleCompletion(completion: completion)
-            }, receiveValue: { [weak self]image in
-                self?.image = image
-                self?.imageSubscription?.cancel()
+            }, receiveValue: { [weak self]returnedImage in
+                guard let self = self else {return}
+                self.image = returnedImage
+                self.imageSubscription?.cancel()
+                self.localFileManger.saveImage(image: returnedImage ?? UIImage(), imageName: self.imageName, folderName: self.folderName)
             })
     }
 }
